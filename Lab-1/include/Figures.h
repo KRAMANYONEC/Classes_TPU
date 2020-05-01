22cc715ea2d79007b00b9572fb9059758349803f
Ivan Kramoykin <ivankramoykin@gmail.com>
2020-05-01 10:19:29 +0100

    Final version of the first lab about geometric figures.

--- Lab-1/include/Figures.h
+++ Lab-1/include/Figures.h
@@ -1,1 +1,32 @@
- 
+#include <iostream>
+
+class Figures 
+{
+public:
+    Figures(double &, double &  // Coordinates of pentagon's center
+          , double &, double &  // Coordinates of one of pentagon's vertexes
+          , double &);  // radius of circumscribed circle
+    ~Figures ();
+    // Method, calculating coordinates of
+    // undefined vertexes 
+    void calc_coords();
+    // Method, returning 1 if point belongs
+    // to pentagon
+    bool is_point_belong ( double &, double &);
+    // Method, returning 1 if pentagon crosses
+    // the X axis
+    bool is_cross_x();
+    //Method, returning 1 if pentagon crosses 
+    // the Y axis
+    bool is_cross_y();
+private:
+    double _xC;
+    double _yC;
+    double _xV1;
+    double _yV1;
+    double _R;
+    double _vertexCoords [5][2];
+};
+
+    
+    
--- Lab-1/src/main.cpp
+++ Lab-1/src/main.cpp
@@ -1,1 +1,57 @@
- 
+#include <iostream>
+
+#include "../include/Figures.h"
+
+int main () {
+    // Parameters of pentagon
+    double xCenter;
+    double yCenter;
+    double xVertex;
+    double yVertex;
+    double radius;
+    
+    // Some point 
+    double xPoint;
+    double yPoint;
+    
+    // Input pentagon's parameters
+    std::cout << "Введите координату х центра ";
+    std::cin >> xCenter;
+    std::cout << "Введите координату y центра ";
+    std::cin >> yCenter;
+    std::cout << "Введите координату х вершины ";
+    std::cin >> xVertex;
+    std::cout << "Введите координату y вершины ";
+    std::cin >> yVertex;
+    std::cout << "Введите радиус описанной окружности ";
+    std::cin >> radius;
+    
+    // Create an instance of Figures
+    Figures pentangle (xCenter, yCenter
+                     , xVertex, yVertex
+                     , radius);
+    
+    // Calculate other coordinates 
+    // of pentagon's vertexes
+    pentangle.calc_coords();
+    
+    // Input point's parameters
+    std::cout << "Введите координату х точки ";
+    std::cin >> xPoint;
+    std::cout << "Введите координату y точки ";
+    std::cin >> yPoint;
+    
+    if(pentangle.is_point_belong(xPoint, yPoint)) {
+        std::cout << "YES" << '\n';
+    } else std::cout << "NO" << '\n';
+    
+    if (pentangle.is_cross_x()) {
+        std::cout << "Figure crosses the X axis" << '\n';
+    } else std::cout << "Figure doesn't cross the X axis" << '\n';
+    
+     if (pentangle.is_cross_y()) {
+        std::cout << "Figure crosses the Y axis" << '\n';
+    } else std::cout << "Figure doesn't cross the Y axis" << '\n';
+        
+    return 0;
+}
